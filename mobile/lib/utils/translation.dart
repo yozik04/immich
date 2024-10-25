e812703a0ce3eb52661a92d2ed435109d14b722f
ben-basten <45583362+ben-basten@users.noreply.github.com>
2024-10-24 20:37:53 -0400

    wip: slang POC

--- mobile/lib/utils/translation.dart
+++ mobile/lib/utils/translation.dart
@@ -1,14 +1,12 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:intl/message_format.dart';
 
-String t(String key, [Map<String, Object>? args]) {
+String tr(String message, [Map<String, Object>? args]) {
   try {
-    String message = key.tr();
     if (args != null) {
       return MessageFormat(message).format(args);
     }
     return message;
   } catch (e) {
-    return key;
+    return message;
   }
 }
